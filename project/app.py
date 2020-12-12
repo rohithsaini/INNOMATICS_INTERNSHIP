3714277386cfa0572d32c09bbc23316d31b56b15
rohithsaini <42429666+rohithsaini@users.noreply.github.com>
2020-12-12 11:25:09 +0530

    Add files via upload

---
+++ project/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app =Flask(__name__)
+
+@app.route('/')
+def info():
+    return render_template('resume.html')
+
+if (__name__ == '__main__'):
+    app.run(debug=True)
